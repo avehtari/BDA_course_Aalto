dd3a46dbdafd78bb46c2b126e7e4846b2a5f8794
Måns Magnusson <mons.magnusson@gmail.com>
2019-09-03 09:28:25 +0300

    small fixes

--- rpackage/data-raw/windshieldy.R
+++ rpackage/data-raw/windshieldy.R
@@ -1,3 +1,3 @@
-windshieldy1 <- read.table("data-raw/windshieldy1.txt")
-windshieldy2 <- read.table("data-raw/windshieldy2.txt")
-usethis::use_data(windshieldy1, windshieldy2)
+windshieldy1 <- read.table("data-raw/windshieldy1.txt")$V1
+windshieldy2 <- read.table("data-raw/windshieldy2.txt")$V1
+usethis::use_data(windshieldy1, windshieldy2, overwrite = TRUE)
