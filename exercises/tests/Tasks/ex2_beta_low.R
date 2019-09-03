413fbe667a02f22d394ef41e1e7663d28125ee51
Måns Magnusson <mons.magnusson@gmail.com>
2019-09-03 12:32:44 +0300

    Added test cases

---
+++ exercises/tests/Tasks/ex2_beta_low.R
@@ -0,0 +1,35 @@
+context("beta_low()")
+
+test_that("beta_low()", {  
+  
+  #-----------------------------------------------------------------------------------
+  # test for beta_low()
+  #-----------------------------------------------------------------------------------
+  expect_true(exists("beta_low"),
+              info = "Error: beta_low() is missing")
+  
+  test <- c(0, 1, 1, 0, 1, 1)
+  alpha <- 2
+  beta <- 4
+  expect_equal(beta_low(prior_alpha = alpha, prior_beta = beta, data = test, pi_0 = 0.5), 
+               c(0.5), tol= 0.001, 
+               info = "Error: Incorrect result for prior_alpha = 2, prior_beta = 4 and data = c(0, 1, 1, 0, 1, 1)")
+  
+  test <- c(0, 1, 1, 0, 1, 1, 0, 1)
+  alpha <- 3
+  beta <- 5
+  expect_equal(beta_low(prior_alpha = alpha, prior_beta = beta, data = test, pi_0 = 0.5), 
+               c(0.5), tol= 0.001, 
+               info = "Error: Incorrect result for prior_alpha = 3, prior_beta = 5 and data = c(0, 1, 1, 0, 1, 1, 0, 1)")
+
+  test <- c(0, 1, 1, 0, 1, 1, 0, 1, 1, 1)
+  alpha <- 3
+  beta <- 5
+  expect_equal(beta_low(prior_alpha = alpha, prior_beta = beta, data = test, pi_0 = 0.2), 
+               0.4572175, tol= 0.001, 
+               info = "Error: Incorrect result for prior_alpha = 2, prior_beta = 4 and data = c(0, 1, 1, 0, 1, 1, 0, 1, 1, 1)")
+  
+})
+
+
+
---
+++ exercises/tests/Tasks/ex2_beta_point_est.R
@@ -0,0 +1,35 @@
+context("beta_point_est()")
+
+test_that("beta_point_est()", {  
+  
+  #-----------------------------------------------------------------------------------
+  # test for beta_point_est()
+  #-----------------------------------------------------------------------------------
+  expect_true(exists("beta_point_est"),
+              info = "Error: beta_point_est() is missing")
+  
+  test <- c(0, 1, 1, 0, 1, 1)
+  alpha <- 2
+  beta <- 4
+  expect_equal(beta_point_est(prior_alpha = alpha, prior_beta = beta, data = test), 
+               c(0.5), tol= 0.001, 
+               info = "Error: Incorrect result for prior_alpha = 2, prior_beta = 4 and data = c(0, 1, 1, 0, 1, 1)")
+  
+  test <- c(0, 1, 1, 0, 1, 1, 0, 1)
+  alpha <- 3
+  beta <- 5
+  expect_equal(beta_point_est(prior_alpha = alpha, prior_beta = beta, data = test), 
+               c(0.5), tol= 0.001, 
+               info = "Error: Incorrect result for prior_alpha = 3, prior_beta = 5 and data = c(0, 1, 1, 0, 1, 1, 0, 1)")
+
+  test <- c(0, 1, 1, 0, 1, 1, 0, 1, 1, 1)
+  alpha <- 3
+  beta <- 5
+  expect_equal(beta_point_est(prior_alpha = alpha, prior_beta = beta, data = test), 
+               c(0.5555556), tol= 0.001, 
+               info = "Error: Incorrect result for prior_alpha = 2, prior_beta = 4 and data = c(0, 1, 1, 0, 1, 1, 0, 1, 1, 1)")
+    
+})
+
+
+
