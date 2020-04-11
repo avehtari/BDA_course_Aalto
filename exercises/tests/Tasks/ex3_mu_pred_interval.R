941d2f8197456f65ed7c9df37c9e7074e8f6d93c
Måns Magnusson <mons.magnusson@gmail.com>
2020-04-11 21:12:51 +0300

    Fixed exercise 3

--- exercises/tests/Tasks/ex3_mu_pred_interval.R
+++ exercises/tests/Tasks/ex3_mu_pred_interval.R
@@ -7,11 +7,16 @@ test_that("mu_pred_interval()", {
   #-----------------------------------------------------------------------------------
   expect_true(exists("mu_pred_interval"),
               info = "Error: mu_pred_interval() is missing")
+  checkmate::expect_function(mu_pred_interval, args = c("data", "prob"), 
+                             info = "Incorrect function arguments.")
   
   test1 <- c(13.3, 14.9, 14.8, 14.8)
-  expect_equal(unname(mu_pred_interval(test1, 0.95)), c(11.7, 17.2), tol= 0.01, info = "Error: Incorrect result for  c(13.3, 14.9, 14.8, 14.8), (95%)")
-  expect_equal(unname(mu_pred_interval(1:10, 0.95)), c(-1.7, 12.7), tol= 0.01, info = "Error: Incorrect result for 1:10, (95%)")
-  expect_equal(unname(mu_pred_interval(1:10, 0.8)), c(1.1, 9.9), tol= 0.01, info = "Error: Incorrect result for 1:10, (80%)")
+  expect_equivalent(mu_pred_interval(test1, 0.95), c(11.7, 17.2), tol= 0.01, 
+                    info = "Error: Incorrect result for  c(13.3, 14.9, 14.8, 14.8), (95%)")
+  expect_equivalent(mu_pred_interval(1:10, 0.95), c(-1.7, 12.7), tol= 0.01, 
+                    info = "Error: Incorrect result for 1:10, (95%)")
+  expect_equivalent(mu_pred_interval(1:10, 0.8), c(1.1, 9.9), tol= 0.01, 
+                    info = "Error: Incorrect result for 1:10, (80%)")
 })
 
 
--- exercises/tests/Tasks/ex3_por_point_est.R
+++ exercises/tests/Tasks/ex3_por_point_est.R
@@ -7,17 +7,22 @@ test_that("posterior_odds_ratio_point_est()", {
   #-----------------------------------------------------------------------------------
   expect_true(exists("posterior_odds_ratio_point_est"),
               info = "Error: posterior_odds_ratio_point_est() is missing")
+  checkmate::expect_function(posterior_odds_ratio_point_est, args = c("p0", "p1"), 
+                             info = "Incorrect function arguments.")  
+  
   set.seed(4711)
   p0 <- rbeta(100000, 5, 95)
   p1 <- rbeta(100000, 10, 90)
-  expect_equal(unname(posterior_odds_ratio_point_est(p0,p1)), 2.676146, tol= 0.01, info = "Error: Incorrect result for p0 = rbeta(100000, 5, 95), p1 = rbeta(100000, 10, 90).")
-  expect_equal(unname(posterior_odds_ratio_point_est(p1,p0)), 0.5307909, tol= 0.01, info = "Error: Incorrect result for p1 = rbeta(100000, 5, 95), p0 = rbeta(100000, 10, 90).")
+  expect_equivalent(posterior_odds_ratio_point_est(p0, p1), 2.676146, tol= 0.01, 
+                    info = "Error: Incorrect result for p0 = rbeta(100000, 5, 95), p1 = rbeta(100000, 10, 90).")
+  expect_equivalent(posterior_odds_ratio_point_est(p1, p0), 0.5307909, tol= 0.01, 
+                    info = "Error: Incorrect result for p1 = rbeta(100000, 5, 95), p0 = rbeta(100000, 10, 90).")
   
   set.seed(4711)
   p0 <- rbeta(100000, 5, 5)
   p1 <- rbeta(100000, 6, 5)
-  expect_equal(unname(posterior_odds_ratio_point_est(p0, p1)), 1.876519, tol= 0.01, info = "Error: Incorrect result for p0 = rbeta(100000, 5, 5), p1 = rbeta(100000, 5, 6).")
-  expect_equal(unname(posterior_odds_ratio_point_est(p1, p0)), 1.24902, tol= 0.01, info = "Error: Incorrect result for p0 = rbeta(100000, 5, 6), p1 = rbeta(100000, 5, 5).")
+  expect_equivalent(posterior_odds_ratio_point_est(p0, p1), 1.876519, tol= 0.01, info = "Error: Incorrect result for p0 = rbeta(100000, 5, 5), p1 = rbeta(100000, 5, 6).")
+  expect_equivalent(posterior_odds_ratio_point_est(p1, p0), 1.24902, tol= 0.01, info = "Error: Incorrect result for p0 = rbeta(100000, 5, 6), p1 = rbeta(100000, 5, 5).")
   
 })
 
