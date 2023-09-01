0f4de0b24f5d849f0c5b4f083da74d2bba59d7d5
Nikolas Siccha <nikolas.siccha@aalto.fi>
2023-09-01 14:25:10 +0300

    update github action to use publish

---
+++ fetch_assignments.sh
@@ -0,0 +1,8 @@
+OUTPUT_DIR="${1:-"assignment_templates"}"
+mkdir -p $OUTPUT_DIR
+cd $OUTPUT_DIR
+wget https://avehtari.github.io/BDA_course_Aalto/assignments/templates.zip
+unzip templates.zip
+chmod -R a+rX .
+rm templates.zip
+cd ..
